2181cd2bd20a1e30fa8ebdce7e979a64ddb63e7e
xiao <2668025394@qq.com>
2021-01-06 17:30:10 +0800

    111

---
+++ src/main/java/com/gitlearn/tetet.java
@@ -0,0 +1,12 @@
+package com.gitlearn;
+
+/**
+ * @author X
+ * @date 2021/1/6 17:28
+ */
+public class tetet {
+    public static void main(String[] args) {
+        System.out.println("11");
+
+    }
+}
